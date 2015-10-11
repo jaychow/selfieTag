f3c8fa388c1685f7915aee2a2cd79a68af136160
jaychow <jay.chow@katanaconnect.com>
2015-10-11 08:10:09 -0700

    map mode

--- resources/views/frontend/igmine/london.blade.php
+++ resources/views/frontend/igmine/london.blade.php
@@ -2,20 +2,22 @@
 
 @section('content')
     {!! csrf_field() !!}
-    <div class="row">
-        @foreach($images as $image)
-            <div class="col-xs-6 col-sm-3 col-md-2 img-item-container">
-                <div class="img-item text-center tag-img {{($image->is_selfie) ? 'active' : ''}}" data-id="{{$image->id}}">
-                    <img class="img-responsive" src="{{$image->low_res}}" />
+    <div class="main-grid">
+        <div class="row">
+            @foreach($images as $image)
+                <div class="col-xs-6 col-sm-3 col-md-2 img-item-container">
+                    <div class="img-item text-center tag-img {{($image->is_selfie) ? 'active' : ''}}" data-id="{{$image->id}}">
+                        <img class="img-responsive" src="{{$image->low_res}}" />
+                    </div>
+                    <div class="text-center">
+                        <a href="{{$image->link}}" class="btn btn-primary btn-block" target="_blank">
+                            View Image <i class="fa fa-photo"></i>
+                        </a>
+                    </div>
                 </div>
-                <div class="text-center">
-                    <a href="{{$image->link}}" class="btn btn-primary btn-block" target="_blank">
-                        View Image <i class="fa fa-photo"></i>
-                    </a>
-                </div>
-            </div>
-        @endforeach
+            @endforeach
 
+        </div>
     </div>
 @endsection
 
