04787d33ae7dcc85c51856121ada1717c6387806
jaychow <jay.chow@katanaconnect.com>
2015-10-11 04:30:58 -0700

    update style

--- resources/views/frontend/igmine/london.blade.php
+++ resources/views/frontend/igmine/london.blade.php
@@ -4,12 +4,12 @@
     {!! csrf_field() !!}
     <div class="row">
         @foreach($images as $image)
-            <div class="col-xs-6 col-sm-3 col-md-2">
+            <div class="col-xs-6 col-sm-3 col-md-2 img-item-container">
                 <div class="img-item text-center tag-img {{($image->is_selfie) ? 'active' : ''}}" data-id="{{$image->id}}">
                     <img class="img-responsive" src="{{$image->low_res}}" />
                 </div>
                 <div class="text-center">
-                    <a href="{{$image->link}}" class="btn btn-primary" target="_blank">
+                    <a href="{{$image->link}}" class="btn btn-primary btn-block" target="_blank">
                         View Image <i class="fa fa-photo"></i>
                     </a>
                 </div>
